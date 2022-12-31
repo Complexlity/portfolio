841ccbaf3fc1fdc9d27320c142a77e30ae96d37a
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-31 22:44:23 +0100

    Add Projects toggle button

--- components/Projects/AllProjects.tsx
+++ components/Projects/AllProjects.tsx
@@ -1,14 +1,18 @@
 import projects from "./projects";
 import Card from "./Card";
+import Button from "../Button";
 
 const Projects = () => {
   return (
-    <div className="px-3">
+    <div className="space-y-4 px-3">
       <h2 className="mb-3 font-bold">Projects</h2>
       <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3 ">
         {projects.map((project) => (
           <Card key={project.id} card={project} />
         ))}
+      </div>
+      <div className="mx-auto grid w-[40%]">
+        <Button text="SEE ALL PROJECTS" />
       </div>
     </div>
   );
