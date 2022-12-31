f479b2c79986c00bb17ce8c9429a7bd294ca363f
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-31 22:41:28 +0100

    Change grid layout

--- components/Projects/AllProjects.tsx
+++ components/Projects/AllProjects.tsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div className="px-3">
       <h2 className="mb-3 font-bold">Projects</h2>
-      <div className="grid gap-4 md:grid-cols-2 ">
+      <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3 ">
         {projects.map((project) => (
           <Card key={project.id} card={project} />
         ))}
