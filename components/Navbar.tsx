8fead74690caadbc8ee34a86925d4f994a6154fe
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-02 19:55:40 +0100

    Create mobile navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,15 +5,17 @@ import { useState } from "react";
 
 import Image from "next/image";
 
-const linkStyles = `py-2 bg-orange-500 border-b-2 border-black`;
-
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
-  const navTranslate = isOpen ? `0px` : `-130%`;
 
+  const navTranslate = isOpen ? `0px` : `-20rem`;
+  const animation = {
+    transition: `all .5s ease-in-out`,
+    transform: `translateY(${navTranslate})`,
+  };
   return (
-    <nav className="flex flex-col justify-between md:flex-row ">
-      <div className="flex justify-between">
+    <nav className="sticky top-0 flex flex-col justify-between md:flex-row ">
+      <div className="flex items-center justify-between">
         <div className="w-10 md:w-12">
           <Image className="max-h-full w-auto" src={complexIcon} alt="Logo" />
         </div>
@@ -21,26 +23,41 @@ const Navbar = () => {
           <Hamburger toggled={isOpen} toggle={setOpen} />
         </div>
       </div>
-      <div
-        className={`fixed top-12  flex flex-col md:flex-row md:gap-12 translate-y-[${navTranslate}]`}
-      >
-        <Link href="#hero" className={linkStyles}>
-          Home
-        </Link>
-        <Link href="#projects" className={linkStyles}>
-          About
-        </Link>
-        <Link href="#projects" className={linkStyles}>
-          Projects
-        </Link>
-        <Link href="#projects" className={linkStyles}>
-          Skills
-        </Link>
-        <Link href="#projects" className={linkStyles}>
-          Contact
-        </Link>
-      </div>
+      <NavLinks animation={animation} mobile={true} />
+      <NavLinks mobile={false} />
     </nav>
+  );
+};
+
+type navlinks = {
+  animation?: { transform: string; transition: string };
+  mobile: boolean;
+};
+
+const NavLinks = ({ animation, mobile }: navlinks) => {
+  const linkStyles = mobile ? `py-2 bg-gray-200  border-b-2 border-black` : "";
+
+  const wrapperStyles = mobile
+    ? `absolute right-0 left-0 top-12 flex flex-col bg-gray-300 pb-2 md:hidden`
+    : `hidden gap-8 py-4 uppercase md:flex`;
+  return (
+    <div style={animation} className={wrapperStyles}>
+      <Link href="#hero" className={linkStyles}>
+        Home
+      </Link>
+      <Link href="#projects" className={linkStyles}>
+        About
+      </Link>
+      <Link href="#projects" className={linkStyles}>
+        Projects
+      </Link>
+      <Link href="#projects" className={linkStyles}>
+        Skills
+      </Link>
+      <Link href="#projects" className={linkStyles}>
+        Contact
+      </Link>
+    </div>
   );
 };
 
