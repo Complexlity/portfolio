c0eda90652eb72048f62fe5aa2c055b75079e3db
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-05 00:28:28 +0100

    Add theme context provider to root app layer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import ThemeContextProvider from "./Contexts/ThemeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeContextProvider>
+      <Component {...pageProps} />
+    </ThemeContextProvider>
+  );
 }
