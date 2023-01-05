c34e5080031136a0dc77b532adc1f4fafdea548f
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-05 17:21:54 +0100

    Add projects gifs

---
+++ Contexts/ThemeContext.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect, createContext, FC, useContext } from "react";
+
+export type GlobalTheme = {
+  darkMode: boolean;
+  setDarkMode: (c: boolean) => void;
+};
+const ThemeContext = createContext<GlobalTheme>({
+  darkMode: false,
+  setDarkMode: () => {},
+});
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const useThemeContext = () => useContext(ThemeContext);
+
+const ThemeContextProvider = ({ children }: Props) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContextProvider;
