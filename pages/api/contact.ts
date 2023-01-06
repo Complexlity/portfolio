a2e3306ad885005c7f770af75ede17decfc0c9bd
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-06 20:50:44 +0100

    Complete send mail function

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,12 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { validateHeaderValue } from 'http'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { mailOPtions, transporter } from '../../util/nodemailer'
 
 type Data = {
   message: string
+  success?: boolean
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -14,7 +16,23 @@ export default function handler(
     const data = req.body
     if (!data.name || !data.email || !data.subject || !data.message)
       return res.status(400).json({ message: "Bad request" })
+
+
+    try {
+      await transporter.sendMail({
+        ...mailOPtions,
+        subject: data.subject,
+        text: `
+        Name : ${data.name}
+        Email: ${data.email}
+        Message: ${data.message}
+        `
+
+      })
+      return res.status(200).json({ message: "posting successful", success: true })
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message })
+
+    }
   }
-  return res.status(200).json({ message: "posting successful" })
-  // return res.status(400).json({ message: "Bad request" })
 }
