0eaf3518eb39a7f980d9092394e241c3346beac5
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-06 19:58:51 +0100

    Add process variables to nodemailer transport

--- util/nodemailer.ts
+++ util/nodemailer.ts
@@ -1,2 +1,16 @@
 import nodemailer from "nodemailer"
-export const transporter = nodemailer.createTransport
+const email = process.env.EMAIL
+const password = process.env.EMAIL_PASSWORD
+
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: email,
+        pass: password
+    }
+})
+
+export const mailOPtions = {
+    from: email,
+    to: email,
+}
