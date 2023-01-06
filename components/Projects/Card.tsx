12b2ff7a30b4451ed392d9686c1ef11d48d165c8
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-06 16:52:57 +0100

    Make gifs unoptimized

--- components/Projects/Card.tsx
+++ components/Projects/Card.tsx
@@ -17,6 +17,7 @@ const Card: FC<CardsProps> = ({ card }) => {
     <div className="group grid overflow-hidden rounded-lg shadow-md duration-75 ease-in-out hover:scale-[101%] dark:shadow-orange-800">
       <div>
         <Image
+          unoptimized={true}
           className="w-full"
           src={card.image || projectImg}
           alt="projectImage"
@@ -24,7 +25,7 @@ const Card: FC<CardsProps> = ({ card }) => {
           height={400}
         />
       </div>
-      <div className="grid gap-2 py-4 px-6">
+      <div className="projectGrid grid gap-2 py-4 px-6">
         <h1 className="text-xl font-semibold uppercase dark:text-gray-200">
           {card.title}
         </h1>
@@ -38,6 +39,7 @@ const Card: FC<CardsProps> = ({ card }) => {
           ))}
         </div>
         <p className="dark:text-gray-200">{card.description}</p>
+
         <div className="absolute inset-0  hidden items-center justify-center gap-8 bg-gradient-to-r from-blue-700/95 to-sky-800/95 group-hover:flex dark:from-green-600/95 dark:to-cyan-900/95">
           <Link href={card.links.live}>
             <TbWorld
