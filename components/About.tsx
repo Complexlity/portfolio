799c6767a3715d782c46441f0b85a605a8d98088
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-07 09:59:09 +0100

    Adjust mobile scroll margin top

--- components/About.tsx
+++ components/About.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Image from "next/image";
-import AboutImg from "../assets/hero-image.png";
 
 const About = () => {
   return (
-    <section id="about" className="flex w-full items-center px-2 md:h-screen">
+    <section
+      id="about"
+      className="mt-2 flex w-full items-center px-2 md:h-screen"
+    >
       <div className="  gap-8 md:grid">
         <div className="">
           <h2 className=" text-xl font-bold  uppercase tracking-widest text-primary dark:text-orange-300">
@@ -20,13 +21,13 @@ const About = () => {
             <p className="py-2 text-gray-600 dark:text-gray-100">
               I specialize in building mobile responsive front-end UI
               applications that connect with API&apos;s and other backend
-              technologies. I&apos;m passionate about learning new technologies and
-              understand there is more than one way to accomplish a task. Though
-              I am most proficient in building front-end applications using
-              HTML, CSS, Javascript, and React, I am a quick learner and can
-              pick up new tech stacks as needed. I believe that being a great
-              developer is not using one specific language, but choosing the
-              best tool for the job.
+              technologies. I&apos;m passionate about learning new technologies
+              and understand there is more than one way to accomplish a task.
+              Though I am most proficient in building front-end applications
+              using HTML, CSS, Javascript, and React, I am a quick learner and
+              can pick up new tech stacks as needed. I believe that being a
+              great developer is not using one specific language, but choosing
+              the best tool for the job.
             </p>
             <p className="py-2 text-gray-600 dark:text-gray-100">
               I started web developement in 2013 managing multiple e-commerce
