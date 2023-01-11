0eb46afe1a45788a54e5d063e56e5f30d69a9904
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-11 15:24:05 +0100

    Resolve link stylesheets error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import ThemeContextProvider from "../Contexts/ThemeContext";
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/gh/loadingio/ldbutton@v1.0.1/dist/ldbtn.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/gh/loadingio/loading.css@v2.0.0/dist/loading.css"
+          rel="stylesheet"
+        />
+      </Head>
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,600;1,700&display=swap"
@@ -12,6 +21,7 @@ export default function Document() {
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
       />
+
       <body>
         <Main />
         <NextScript />
