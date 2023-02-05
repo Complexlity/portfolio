aaf393f080781119987336ba4e7068fb912de807
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-02-05 14:50:40 +0100

    Complete toast message

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,12 +1,19 @@
 import { FormEvent, useState } from "react";
 import { sendContactForm } from "../mail-files/sendForm";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRef } from "react";
 export interface formValues {
   name: string;
   email: string;
   subject: string;
   message: string;
 }
+
+export type result = {
+  message: string;
+  success?: boolean;
+};
 
 const emptyformValues: formValues = {
   name: "",
@@ -18,6 +25,21 @@ const emptyformValues: formValues = {
 const Contact = () => {
   const [values, setValues] = useState<formValues>(emptyformValues);
   const [loading, setLoading] = useState<boolean>(false);
+  const toastId: any = useRef(null);
+  const notify = () =>
+    (toastId.current = toast.info("Sending...", {
+      autoClose: false,
+      icon: true,
+    }));
+  const update = (result: result) => {
+    let type = result.success ? toast.TYPE.SUCCESS : toast.TYPE.ERROR;
+    let autoCloseTime = result.success ? 2000 : 4000;
+    toast.update(toastId.current, {
+      render: result.message,
+      type,
+      autoClose: autoCloseTime,
+    });
+  };
 
   async function submitValues(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -25,15 +47,16 @@ const Contact = () => {
       return;
     setValues(emptyformValues);
     setLoading(true);
-    let res;
+    notify();
+    let res: result;
     try {
       let value = await sendContactForm(values);
       res = await value.json();
     } catch (error) {
-      res = { messsage: "Failed: Could not connect" };
+      res = { success: false, message: "Something went wrong 😢" };
     }
     setLoading(false);
-    alert(res.message);
+    update(res);
   }
 
   function updateValues(item: number, newValue: string) {
@@ -66,68 +89,71 @@ const Contact = () => {
   }
 
   return (
-    <section id="contact" className=" mt-8 grid px-3 md:mt-12 ">
-      <h2 className="contact-me mb-6 text-xl uppercase tracking-widest text-primary dark:text-orange-300 ">
-        Connect with me
-      </h2>
-      <form onSubmit={submitValues} className="myForm mx-auto  grid w-full">
-        <input
-          value={values.name}
-          onChange={(e) => updateValues(0, e.target?.value)}
-          className=""
-          type="text"
-          id="name"
-          placeholder="Name"
-          required
-        />
-        <input
-          onChange={(e) => updateValues(1, e.target?.value)}
-          className=""
-          type="email"
-          placeholder="Email"
-          value={values.email}
-          required
-        />
-        <input
-          onChange={(e) => updateValues(2, e.target?.value)}
-          className=""
-          type="text"
-          placeholder="Subject"
-          value={values.subject}
-          required
-        />
-        <textarea
-          onChange={(e) => updateValues(3, e.target?.value)}
-          className=""
-          name="message"
-          placeholder="Message"
-          id="message"
-          cols={30}
-          rows={5}
-          value={values.message}
-          required
-        ></textarea>
-        <div className="mx-auto w-full max-w-[400px] text-center">
-          {!loading ? (
-            <button className="md:text-md w-full   rounded-[2rem] bg-primary px-2 py-[.8rem] uppercase  text-gray-100 hover:bg-primaryLight dark:bg-orange-300 dark:text-[80%] dark:font-semibold dark:text-black hover:dark:bg-orange-500   md:tracking-wide">
-              {" "}
-              Send Message
-            </button>
-          ) : (
-            <button
-              disabled
-              className="md:text-md w-full   rounded-[2rem] bg-primaryLight px-2 py-[.8rem] uppercase  text-gray-100 hover:bg-primaryLight dark:bg-orange-300 dark:text-[80%] dark:font-semibold dark:text-black hover:dark:bg-orange-500   md:tracking-wide"
-            >
-              {" "}
-              <span className="loader ld-ext-right running flex items-center justify-center">
-                <span>Sending</span>{" "}
-                <span className="ld ld-ring ld-spin ml-2"></span>{" "}
-              </span>
-            </button>
-          )}
-        </div>
-      </form>
-    </section>
+    <>
+      <ToastContainer position="top-center" />
+      <section id="contact" className=" mt-8 grid px-3 md:mt-12 ">
+        <h2 className="contact-me mb-6 text-xl uppercase tracking-widest text-primary dark:text-orange-300 ">
+          Connect with me
+        </h2>
+        <form onSubmit={submitValues} className="myForm mx-auto  grid w-full">
+          <input
+            value={values.name}
+            onChange={(e) => updateValues(0, e.target?.value)}
+            className=""
+            type="text"
+            id="name"
+            placeholder="Name"
+            required
+          />
+          <input
+            onChange={(e) => updateValues(1, e.target?.value)}
+            className=""
+            type="email"
+            placeholder="Email"
+            value={values.email}
+            required
+          />
+          <input
+            onChange={(e) => updateValues(2, e.target?.value)}
+            className=""
+            type="text"
+            placeholder="Subject"
+            value={values.subject}
+            required
+          />
+          <textarea
+            onChange={(e) => updateValues(3, e.target?.value)}
+            className=""
+            name="message"
+            placeholder="Message"
+            id="message"
+            cols={30}
+            rows={5}
+            value={values.message}
+            required
+          ></textarea>
+          <div className="mx-auto w-full max-w-[400px] text-center">
+            {!loading ? (
+              <button className="md:text-md w-full   rounded-[2rem] bg-primary px-2 py-[.8rem] uppercase  text-gray-100 hover:bg-primaryLight dark:bg-orange-300 dark:text-[80%] dark:font-semibold dark:text-black hover:dark:bg-orange-500   md:tracking-wide">
+                {" "}
+                Send Message
+              </button>
+            ) : (
+              <button
+                disabled
+                className="md:text-md w-full   rounded-[2rem] bg-primaryLight px-2 py-[.8rem] uppercase  text-gray-100 hover:bg-primaryLight dark:bg-orange-300 dark:text-[80%] dark:font-semibold dark:text-black hover:dark:bg-orange-500   md:tracking-wide"
+              >
+                {" "}
+                <span className="loader ld-ext-right running flex items-center justify-center">
+                  <span>Sending</span>{" "}
+                  <span className="ld ld-ring ld-spin ml-2"></span>{" "}
+                </span>
+              </button>
+            )}
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "../styles/loader.css";
 import type { AppProps } from "next/app";
 import ThemeContextProvider from "../Contexts/ThemeContext";
 
