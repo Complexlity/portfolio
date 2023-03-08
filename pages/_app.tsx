df47e0901f318e708699078aad372dd7f05ae57f
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-03-08 16:36:39 +0100

    feat: add analytics to top level component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import "../styles/loader.css";
 import "../styles/swiper.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import ThemeContextProvider from "../Contexts/ThemeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeContextProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeContextProvider>
   );
 }
