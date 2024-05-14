e2f1f7fdc2f98cc2c5a080ec3321a4885faf049f
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-05-14 09:38:26 +0100

    style: update button

--- components/Framespolio/Frame.tsx
+++ components/Framespolio/Frame.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FramesType } from "./data";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 export default function Frame({ frame, index }: { frame: FramesType, index: number }) {
   return (
     <>
@@ -20,10 +22,14 @@ export default function Frame({ frame, index }: { frame: FramesType, index: numb
               {frame.description}
             </p>
             <Link
-              className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+              className="flex w-fit mx-auto gap-2 justify-between h-10 relative items-center rounded-md bg-gray-900 px-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
               href={frame.link}
+              target="_blank"
             >
-              See Preview
+              <span>Preview</span>
+              <span >
+                <FaExternalLinkAlt />
+              </span>
             </Link>
           </div>
           <Image
